c4c2785ae93e525991b7e807de0023fc92a35718
Danil <ynx-gaan-d@yandex.ru>
2025-10-05 01:25:29 +0500

    Fix checkstyle

--- stats/stats-server/src/main/java/ewm/utils/DateFormatConfig.java
+++ stats/stats-server/src/main/java/ewm/utils/DateFormatConfig.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.time.format.DateTimeFormatter;
 
