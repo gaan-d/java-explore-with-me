2a97626d74df40ec2f25a1c9aff2ea918ffd14bc
Danil <ynx-gaan-d@yandex.ru>
2025-10-07 16:09:14 +0500

    Fix review remarks

--- main-service/src/main/java/ewm/exception/ErrorHandler.java
+++ main-service/src/main/java/ewm/exception/ErrorHandler.java
@@ -3,6 +3,8 @@ package ewm.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -61,7 +63,8 @@ public class ErrorHandler {
                 .build();
     }
 
-    @ExceptionHandler({ValidationException.class, NumberFormatException.class, BadRequestException.class})
+    @ExceptionHandler({ValidationException.class, NumberFormatException.class, BadRequestException.class,
+            MethodArgumentNotValidException.class, MissingServletRequestParameterException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequestException(RuntimeException e) {
         log.error("Некорректный запрос {}", e.getMessage());
